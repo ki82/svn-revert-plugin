a2fe1d1a67ff614a6b0f9815fb4bb3348358afff
Christian Åkerström <akerstrom.christian@gmail.com>
2012-01-26 13:30:49 +0100

    Added dummy config/global jelly files.

--- src/main/java/jenkins/plugins/svn_revert/SvnRevertPublisher.java
+++ src/main/java/jenkins/plugins/svn_revert/SvnRevertPublisher.java
@@ -1,14 +1,55 @@
 package jenkins.plugins.svn_revert;
 
+import hudson.Extension;
+import hudson.model.AbstractProject;
+import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
+import hudson.tasks.Publisher;
+
+import org.kohsuke.stapler.DataBoundConstructor;
 
 public class SvnRevertPublisher extends Notifier {
 
+    private final String revertMessage;
+
     @Override
     public BuildStepMonitor getRequiredMonitorService() {
-        // TODO Auto-generated method stub
+        System.out.println("REVERTING");
         return null;
     }
 
+    public String getRevertMessage() {
+        return "Revert message";
+    }
+
+    @DataBoundConstructor
+    public SvnRevertPublisher(final String revertMessage) {
+        this.revertMessage = revertMessage;
+    }
+
+    @Override
+    public SvnRevertDescriptorImpl getDescriptor() {
+        return (SvnRevertDescriptorImpl)super.getDescriptor();
+    }
+
+
+    @Extension
+    public static final class SvnRevertDescriptorImpl extends BuildStepDescriptor<Publisher> {
+
+        @Override
+        public boolean isApplicable(final Class<? extends AbstractProject> arg0) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Display name";
+        }
+
+        public String getRevertMessage() {
+            return "Revert message 2";
+        }
+
+    }
 }
