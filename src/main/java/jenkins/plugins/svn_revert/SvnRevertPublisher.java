3ec020b5651fae48e5198a4f2a33f60cbeea77e5
Christian Åkerström <akerstrom.christian@gmail.com>
2012-01-26 09:31:54 +0100

    Added empty implementation of SvnRevertPublisher

---
+++ src/main/java/jenkins/plugins/svn_revert/SvnRevertPublisher.java
@@ -0,0 +1,14 @@
+package jenkins.plugins.svn_revert;
+
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
+
+public class SvnRevertPublisher extends Notifier {
+
+    @Override
+    public BuildStepMonitor getRequiredMonitorService() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
