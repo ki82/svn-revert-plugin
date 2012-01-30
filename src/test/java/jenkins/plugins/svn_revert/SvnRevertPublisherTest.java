70d1f22cca86c612695df22a2388a05bf4e597f3
Christian Åkerström <akerstrom.christian@gmail.com>
2012-01-30 12:37:03 +0100

    Added testcases

--- src/test/java/jenkins/plugins/svn_revert/SvnRevertPublisherTest.java
+++ src/test/java/jenkins/plugins/svn_revert/SvnRevertPublisherTest.java
@@ -1,5 +1,20 @@
 package jenkins.plugins.svn_revert;
 
-public class SvnRevertPublisherTest {
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import hudson.model.AbstractBuild;
 
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class SvnRevertPublisherTest extends AbstractMockitoTestCase {
+
+    @Mock
+    AbstractBuild<?, ?> build;
+
+    @Test
+    public void shouldReturnTrueWhenPerformingBuild() throws Exception {
+        final SvnRevertPublisher publisher = new SvnRevertPublisher("");
+        assertThat(publisher.perform(build, null, null), is(true));
+    }
 }
