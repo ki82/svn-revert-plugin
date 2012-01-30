4b0a868b228b7eeb792881692201dc4953e06c1b
Christian Åkerström <akerstrom.christian@gmail.com>
2012-01-30 14:40:54 +0100

    Added implementation of Messenger.

--- src/main/java/jenkins/plugins/svn_revert/Messenger.java
+++ src/main/java/jenkins/plugins/svn_revert/Messenger.java
@@ -4,16 +4,22 @@ import java.io.PrintStream;
 
 class Messenger {
 
-    void informBuildStatusNotUnstable() {
-
-    }
+    static final String BUILD_STATUS_NOT_UNSTABLE =
+            "Will not revert since build status is not UNSTABLE.";
+    static final String PREVIOUS_BUILD_STATUS_NOT_SUCCESS =
+            "Will not revert since previous build status is not SUCCESS.";
+    private PrintStream logger;
 
     void setLogger(final PrintStream logger) {
+        this.logger = logger;
+    }
 
+    void informBuildStatusNotUnstable() {
+        logger.println(BUILD_STATUS_NOT_UNSTABLE);
     }
 
     void informPreviousBuildStatusNotSuccess() {
-
+        logger.println(PREVIOUS_BUILD_STATUS_NOT_SUCCESS);
     }
 
 }
---
+++ src/test/java/jenkins/plugins/svn_revert/MessengerTest.java
@@ -0,0 +1,34 @@
+package jenkins.plugins.svn_revert;
+
+import static org.mockito.Mockito.verify;
+
+import java.io.PrintStream;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class MessengerTest extends AbstractMockitoTestCase {
+
+    @Mock
+    private PrintStream logger;
+    private Messenger messenger;
+
+    @Before
+    public void setup() {
+        messenger = new Messenger();
+        messenger.setLogger(logger);
+    }
+
+    @Test
+    public void logsNotUnstableMessage() throws Exception {
+        messenger.informBuildStatusNotUnstable();
+        verify(logger).println(Messenger.BUILD_STATUS_NOT_UNSTABLE);
+    }
+
+    @Test
+    public void logsPreviousNotSuccessMessage() throws Exception {
+        messenger.informPreviousBuildStatusNotSuccess();
+        verify(logger).println(Messenger.PREVIOUS_BUILD_STATUS_NOT_SUCCESS);
+    }
+}
