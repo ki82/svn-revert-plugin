ca6d226c0740727010739628342810acd801993a
David Pärsson <david.parsson@gmail.com>
2012-02-03 14:30:15 +0100

    Refactoring

--- src/main/java/jenkins/plugins/svn_revert/SvnReverter.java
+++ src/main/java/jenkins/plugins/svn_revert/SvnReverter.java
@@ -21,6 +21,7 @@ public class SvnReverter {
         if (!(rootProject.getScm() instanceof SubversionSCM)) {
             messenger.informNotSubversionSCM();
         }
+
         return true;
     }
 
--- src/test/java/jenkins/plugins/svn_revert/SvnReverterTest.java
+++ src/test/java/jenkins/plugins/svn_revert/SvnReverterTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
+import hudson.scm.SubversionSCM;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,18 +21,18 @@ public class SvnReverterTest extends AbstractMockitoTestCase {
     private AbstractBuild build;
     @Mock
     private AbstractBuild rootBuild;
-
     @Mock
-    private AbstractProject rootproject;
-
+    private AbstractProject rootProject;
     @Mock
     private AbstractProject project;
+    @Mock
+    private SubversionSCM subversionScm;
 
     @Before
     public void setup() {
         when(build.getRootBuild()).thenReturn(rootBuild);
         when(build.getProject()).thenReturn(project);
-        when(project.getRootProject()).thenReturn(rootproject);
+        when(project.getRootProject()).thenReturn(rootProject);
         reverter = new SvnReverter(build, messenger);
     }
 
