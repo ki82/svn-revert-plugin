81e2dee57e921dafee9dd481669b3444f5509589
Christian Åkerström <akerstrom.christian@gmail.com>
2012-02-09 08:23:53 +0100

    Added test that it should not try to revert when not Subversion SCM

--- src/test/java/jenkins/plugins/svn_revert/SvnRevertPluginTest.java
+++ src/test/java/jenkins/plugins/svn_revert/SvnRevertPluginTest.java
@@ -1,5 +1,7 @@
 package jenkins.plugins.svn_revert;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
 import hudson.model.FreeStyleBuild;
 import hudson.model.Result;
 import hudson.model.Cause;
@@ -9,6 +11,7 @@ import hudson.scm.NullSCM;
 import java.util.List;
 
 import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.MockBuilder;
 
 public class SvnRevertPluginTest extends HudsonTestCase {
 
@@ -27,16 +30,21 @@ public class SvnRevertPluginTest extends HudsonTestCase {
         assertBuildStatus(Result.SUCCESS, build);
     }
 
-//    public void testShouldNotRevertWhenNotSubversionSCM() throws Exception {
-//        final FreeStyleProject job = createFreeStyleProject("no-scm-job");
-//        job.setScm(new NullSCM());
-//        job.getPublishersList().add(new JenkinsGlue(""));
-//
-//        final FreeStyleBuild build = job.scheduleBuild2(0, new Cause.UserCause()).get();
-//
-//        System.out.println(build.getLog(LOG_LIMIT));
-//        assertBuildStatus(Result.SUCCESS, build);
-//
-//    }
+    public void testShouldNotRevertWhenNotSubversionSCM() throws Exception {
+        final FreeStyleProject job = createFreeStyleProject("no-scm-job");
+        job.setScm(new NullSCM());
+        job.getPublishersList().add(new JenkinsGlue(""));
+
+        job.scheduleBuild2(0, new Cause.UserCause()).get();
+
+        job.getBuildersList().add(new MockBuilder(Result.UNSTABLE));
+        final FreeStyleBuild build = job.scheduleBuild2(0, new Cause.UserCause()).get();
+
+        final List<String> log = build.getLog(LOG_LIMIT);
+        System.out.println(log.toString());
+        assertThat(log.toString(), containsString("The Subversion Revert Plugin can only be used with Subversion SCM"));
+        assertBuildStatus(Result.UNSTABLE, build);
+
+    }
 
 }
