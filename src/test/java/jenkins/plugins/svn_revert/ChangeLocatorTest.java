f9a1b16de0d6f14320f3fa5ebdfe2067742de579
David Pärsson <david.parsson@gmail.com>
2012-02-22 16:15:29 +0100

    Added tests and removed stacktrace.

--- src/test/java/jenkins/plugins/svn_revert/ChangeLocatorTest.java
+++ src/test/java/jenkins/plugins/svn_revert/ChangeLocatorTest.java
@@ -9,12 +9,13 @@ import hudson.scm.SubversionSCM;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.tmatesoft.svn.core.SVNException;
 
 import com.google.common.collect.Lists;
 
+@SuppressWarnings("unchecked")
 public class ChangeLocatorTest extends AbstractMockitoTestCase {
 
     private ChangeLocator changeLocator;
@@ -32,22 +33,34 @@ public class ChangeLocatorTest extends AbstractMockitoTestCase {
 
     private final List<Module> moduleLocations = Lists.newArrayList();
 
+    private final List<String> changedFilePaths = Lists.newArrayList();
+
     @Before
     public void setUp() throws Exception {
         moduleLocations.add(module);
         when(locationFinder.getModules(subversionScm)).thenReturn(moduleLocations);
+        when(changedFiles.getRepositoryPathsFor(build)).thenReturn(changedFilePaths);
         changeLocator = new ChangeLocator(build, locationFinder, changedFiles);
     }
 
     @Test
     public void returnsFalseIfAllChangesInWorkspace() throws Exception {
+        when(module.getRepositoryPath(build)).thenReturn("path/to/module");
+        changedFilePaths.add("path/to/module/with_file.txt");
         assertThat(changeLocator.changesOutsideWorkspace(subversionScm), is(false));
     }
 
-    @Ignore
-    @Test(expected = IllegalStateException.class)
-    public void shouldThrowWhenModuleUrlDoesNotMatchRepositoryUrl() throws Exception {
-//        when(moduleLocation).
+    @Test
+    public void shouldReturnTrueWhenModuleThrowsException() throws Exception {
+        when(module.getRepositoryPath(build)).thenThrow(SVNException.class);
+        assertThat(changeLocator.changesOutsideWorkspace(subversionScm), is(true));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenChangesNotInWorkspace() throws Exception {
+        when(module.getRepositoryPath(build)).thenReturn("path/to/module");
+        changedFilePaths.add("otherModule");
+        assertThat(changeLocator.changesOutsideWorkspace(subversionScm), is(true));
     }
 
 }
